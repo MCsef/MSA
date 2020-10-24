d2fce83d940a5d2eed2125e2547bbd7b8ff048a1
MCsef <mary_cociorvan@yahoo.ro>
2020-10-24 19:40:53 +0300

    cofigurations

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+<<<<<<< b292b47fccdb41d20729749fdd15a91b5874a80a
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+=======
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+>>>>>>> cofigurations
 
 export default function App() {
+  console.log("App executed successfuly");
   return (
     <SafeAreaView style={styles.container}>
+<<<<<<< b292b47fccdb41d20729749fdd15a91b5874a80a
       <Text>There is still hope!</Text>
+=======
+      <Text>Hello tester! This is a test</Text>
+      <Image source={require('./assets/icon.png')}/>
+>>>>>>> cofigurations
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -14,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'turquoise',
     alignItems: 'center',
     justifyContent: 'center',
   },
