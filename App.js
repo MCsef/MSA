b292b47fccdb41d20729749fdd15a91b5874a80a
AMHategan <49283426+AMHategan@users.noreply.github.com>
2020-10-24 20:00:09 +0300

    configuration process

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>To be continued!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>There is still hope!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
