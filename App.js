6a74db5044e9263d1bb153005df5662e9b80eb5a
MCsef <mary_cociorvan@yahoo.ro>
2020-10-25 13:53:48 +0200

    repaired errors

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-<<<<<<< b292b47fccdb41d20729749fdd15a91b5874a80a
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-=======
 import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
->>>>>>> cofigurations
 
 export default function App() {
   console.log("App executed successfuly");
   return (
     <SafeAreaView style={styles.container}>
-<<<<<<< b292b47fccdb41d20729749fdd15a91b5874a80a
-      <Text>There is still hope!</Text>
-=======
       <Text>Hello tester! This is a test</Text>
       <Image source={require('./assets/icon.png')}/>
->>>>>>> cofigurations
       <StatusBar style="auto" />
     </SafeAreaView>
   );
