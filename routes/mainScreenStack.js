3967e8aa285a1cd3c5d9ca3542127b469fab1f02
MCsef <mary_cociorvan@yahoo.ro>
2020-11-04 17:47:07 +0200

    added login screen and user prfile screen+installed packages for displaying a map

--- routes/mainScreenStack.js
+++ routes/mainScreenStack.js
@@ -3,10 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import MainScreen from "../screens/MainScreen";
 import ContactScreen from "../screens/ContactScreen";
+import LoginScreen from "../screens/LoginScreen";
+import UserProfileScreen from "../screens/UserProfileScreen";
 
 const screens = {
   MainScreen: { screen: MainScreen },
   ContactScreen: { screen: ContactScreen },
+  LoginScreen: { screen: LoginScreen },
+  UserProfileScreen: { screen: UserProfileScreen }
 }
 
 const MainScreenStack=createStackNavigator(screens);
