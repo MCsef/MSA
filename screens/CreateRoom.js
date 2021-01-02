487576f03d20f3ee04f987df6dc07600f44bb767
AMHategan <49283426+AMHategan@users.noreply.github.com>
2021-01-02 14:08:57 +0200

    small updates related to create action for rooms

--- screens/CreateRoom.js
+++ screens/CreateRoom.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {TextInput} from 'react-native-paper';
+import ButtonWithBackground from './ButtonWithBackground';
 
 const CreateRoom = () =>{
     const [title, setTitle] = useState("")
